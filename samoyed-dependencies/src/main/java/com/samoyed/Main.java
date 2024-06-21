dc6b6ba2ca4077a4447b4ff30aac19c504a88766
wyz <isyz.wang@outlook.com>
2024-06-21 16:21:27 +0800

    init

---
+++ samoyed-dependencies/src/main/java/com/samoyed/Main.java
@@ -0,0 +1,12 @@
+package com.samoyed;
+
+/**
+ * @Author wyz
+ * @Date 2024/6/21 16:00
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ samoyed-module-infra/src/main/java/com/samoyed/Main.java
@@ -0,0 +1,12 @@
+package com.samoyed;
+
+/**
+ * @Author wyz
+ * @Date 2024/6/21 15:52
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
